ee2bd493a4434a61a0bb4235e97994b432c6e7e6
Harshita Singh <singhharshita@828gmail.com>
2023-06-09 18:41:56 +0530

    m

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Githubsearch from './components/GithubSearchApp';
+function App() {
+  return (
+    <>
+    <Githubsearch/>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/GithubCredentials.js
@@ -0,0 +1,5 @@
+export const CLIENT_ID = "f8ebbf4785c37cfbf244";
+export const CLIENT_SECRET = "2fc8494f2c6fc86009606c47a2faaa32cb508853";
+
+// export const CLIENT_ID = "c3b7e4c242b30ddef0a8";
+// export const CLIENT_SECRET = "c2479d45d4fb7a54fb16a3b9120e215ab68c98c8";
